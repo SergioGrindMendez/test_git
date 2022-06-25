3f6bfd2111ae607710e8d132973e9f4662dd085a
Guillermo Moreno <guillermo@preyhq.com>
2022-06-24 20:31:33 -0500

    prof-basica & python_intermedio folders added

---
+++ python_intermedio/list_comprehensions.py
@@ -0,0 +1,13 @@
+def main():
+    # squares = []
+    # for i in range(1, 101):
+    #     if i % 3 != 0:
+    #         squares.append(i**2)
+    # variable = [element for element in iterable if conditio]
+    squares = [i**2 for i in range(1, 101) if i % 3 != 0]
+
+    print(squares)
+
+
+if __name__ == "__main__":
+    main()
